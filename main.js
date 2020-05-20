e6938b49c7412ccefefb26cd9cb370ee9deab2c1
jyoti attri <jyotiatri22@gmail.com>
2020-05-20 18:22:05 +0530

    changes in footer

--- main.js
+++ main.js
@@ -571,7 +571,7 @@ Vue.component("footer-component", {
                         <!-- Form -->
                         <div class="footer-form">
                             <div id="mc_embed_signup">
-                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part" novalidate="true">
+                                <form target="_blank" action="#" method="get" class="subscribe_form relative mail_part" novalidate="true">
                                     <input type="email" name="EMAIL" id="newsletter-form-email" placeholder=" Email Address " class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'">
                                     <div class="form-icon">
                                         <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">
@@ -590,16 +590,14 @@ Vue.component("footer-component", {
             <div class="row d-flex justify-content-between align-items-center">
                 <div class="col-xl-9 col-lg-8">
                     <div class="footer-copy-right">
-                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                      <p>All rights are reserved</p>  
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-4">
                     <!-- Footer Social -->
                     <div class="footer-social f-right">
                         <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
                         <a href="#"><i class="fas fa-globe"></i></a>
                         <a href="#"><i class="fab fa-instagram"></i></a>
                     </div>
